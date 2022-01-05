ff35fcbcaf8839ac473b908dce4d501472c6b5ee
VladRidun <ridunvv@gmail.com>
2022-01-05 18:45:38 +0400

    3.1. Переназначение переменной [#232354]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -4,17 +4,19 @@ public class Calculator {
     public static void main(String[] args) {
         int one = 1;
         int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int onePlusTwo = one + two;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
+        int result;
+        result = one + two;
+        System.out.println(result);
+        one= 6;
+        result = one / two;
+        System.out.println(result);
+        one = 5;
+        result = one - two;
+        System.out.println(result);
+        one = two * 2;
+        result = one * two;
+        System.out.println(result);
+
     }
 }
 
