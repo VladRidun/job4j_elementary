a6787861ced740ebe2af453353c6ebf050a3e2da
VladRidun <ridunvv@gmail.com>
2022-01-11 15:37:10 +0400

    5.2. Создать программу, вычисляющую факториал. [#193 #249363]

--- src/test/java/ru/job4j/Loop/FactorialTest.java
+++ src/test/java/ru/job4j/Loop/FactorialTest.java
@@ -14,7 +14,6 @@ public class FactorialTest {
         int expected = 120;
         Assert.assertEquals(expected, result);
     }
-
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
         int n = 0;
