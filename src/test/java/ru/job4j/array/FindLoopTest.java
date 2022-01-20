c7439728fa5d5df84f84379e4fbbe1d0e04332d2
VladRidun <ridunvv@gmail.com>
2022-01-20 10:37:56 +0400

    6.5. Поиск индекса в диапазоне. [#156318 #253278]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -23,20 +23,31 @@ public class FindLoopTest {
     }
 
     @Test
-    public void whenArrayHasLength5Then0() {
-        int[] data = new int[] {5, 10, 3};
-        int el = 5;
+    public void whenArrayHasLength3Then0() {
+        int[] data = new int[] {6, 13, 5};
+        int el = 6;
         int result = FindLoop.indexOf(data, el);
         int expected = 0;
         Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void whenFind3() {
-        int[] data = new int[] {5, 2, 10, 2, 4};
-        int el = 2;
+    public void whenArrayHasLength7ThenMinus1() {
+        int[] data = new int[] {7, 8, 10, 2, 4, 3, 1};
+        int el = 11;
         int start = 2;
         int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasLength7Then3() {
+        int[] data = new int[] {9, 8, 7, 4, 2, 3, 5};
+        int el = 4;
+        int start = 2;
+        int finish = 5;
         int result = FindLoop.indexOf(data, el, start, finish);
         int expected = 3;
         Assert.assertEquals(expected, result);
