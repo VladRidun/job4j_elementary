0492afc1b9e969107dadab07fd9a8da043cdcc47
VladRidun <ridunvv@gmail.com>
2022-01-25 14:24:00 +0400

    6.5.2. Поиск минимума в диапазоне. [#179804 #253289]

--- src/test/java/ru/job4j/array/MinDiapasonTest.java
+++ src/test/java/ru/job4j/array/MinDiapasonTest.java
@@ -23,7 +23,7 @@ public class MinDiapasonTest {
         int start = 1;
         int finish = 3;
         int result = MinDiapason.findMin(array, start, finish);
-        int expected = 3;
+        int expected = 1;
         Assert.assertEquals(expected, result);
     }
 
